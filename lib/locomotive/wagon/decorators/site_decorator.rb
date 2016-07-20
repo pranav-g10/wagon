33fc3c5f55b3cc7a2d80477bd4f44909ef9f5d3b
Pranav Chaturvedi <pranav.g10@gmail.com>
2016-07-20 09:34:12 +0530

    Add is_theme and theme_name fields to site

--- lib/locomotive/wagon/decorators/site_decorator.rb
+++ lib/locomotive/wagon/decorators/site_decorator.rb
@@ -34,14 +34,14 @@ module Locomotive
         end
       end
 
-      %i(robots_txt timezone seo_title meta_keywords meta_description).each do |name|
+      %i(robots_txt timezone seo_title meta_keywords meta_description is_theme theme_name).each do |name|
         define_method(name) do
           self[name]
         end
       end
 
       def __attributes__
-        %i(name handle robots_txt locales timezone seo_title meta_keywords meta_description picture metafields_schema metafields metafields_ui)
+        %i(name handle robots_txt locales timezone seo_title meta_keywords meta_description picture metafields_schema metafields metafields_ui is_theme theme_name)
       end
 
       def edited?
@@ -53,7 +53,7 @@ module Locomotive
     class UpdateSiteDecorator < SiteDecorator
 
       def __attributes__
-        %i(picture locales metafields_schema metafields metafields_ui)
+        %i(picture locales metafields_schema metafields metafields_ui is_theme theme_name)
       end
 
     end
